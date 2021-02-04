39a6a54f9ebdab3a016128df983340779675b0a3
diamondyucas <yucasd@berea.edu>
2021-02-03 19:56:11 -0500

    made food variable able to set at random position

--- Snake Game.py
+++ Snake Game.py
@@ -1,4 +1,5 @@
 import turtle
+from random import randint
 
 wn = turtle.Screen()
 wn.bgcolor("black")
@@ -16,6 +17,7 @@ food = turtle.Turtle()
 food.shape(name="circle")
 food.pencolor("blue")
 food.fillcolor("lightblue")
-
+food.penup()
+food.goto(randint(-100,0), randint(0,100))
 
 wn.exitonclick()
