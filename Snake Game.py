0a2f9b6f04a82f970f887a9e6d982dddacc04edd
diamondyucas <yucasd@berea.edu>
2021-02-03 19:31:44 -0500

    put snake in position

--- Snake Game.py
+++ Snake Game.py
@@ -1,13 +1,15 @@
 import turtle
 wn = turtle.Screen()
-
 wn.bgcolor("black")
 snake = turtle.Turtle()
-
+snake.hideturtle()
+snake.penup()
 snake.shape(name="square")
 snake.fillcolor("pink")
 snake.pencolor("hotpink")
-
+snake.setposition(100,100)
+snake.pendown()
+snake.showturtle()
 
 
 
